64d432f893280042b51550f2c4d3f8080480574c
kabbo25 <kabbo25@gmail.com>
2024-02-19 08:31:19 +0600

    using auxulary array

--- Leetcode/Rearrange-Array-Elements-by-Sign/sol.cpp
+++ Leetcode/Rearrange-Array-Elements-by-Sign/sol.cpp
@@ -3,36 +3,24 @@ using namespace std;
 class Solution {
 public:
   vector<int> rearrangeArray(vector<int> &nums) {
-    deque<int> dq;
-    int n = nums.size(), cnt = 0;
-    for (int i(0); i < n; ++i) {
-      if (nums[i] < 0)
-        dq.emplace_back(nums[i]);
-      else {
-        nums[cnt++] = nums[i];
-      }
-    }
-    bool flag(true);
-    for (int i(1); i < n; ++i) {
-      if (flag) {
-        dq.emplace_back(nums[i]);
-        nums[i] = dq.front();
-        dq.pop_front();
+    int n = nums.size();
+    vector<int> ans(n);
+    int pos(0), neg(1);
+    for (auto x : nums) {
+      if (x > 0) {
+        ans[pos] = x;
+        pos += 2;
       } else {
-        int temp = nums[i];
-        if (dq.back() > 0)
-          nums[i] = dq.back();
-        dq.pop_back();
-        dq.emplace_back(temp);
+        ans[neg] = x;
+        neg += 2;
       }
-      flag ^= 1;
     }
-    return nums;
+    return ans;
   }
 };
 int main() {
   Solution obj;
-  vector<int> v{-1, 1};
+  vector<int> v{3, 1, -2, -5, 2, -4};
   auto p = obj.rearrangeArray(v);
   for (int i = 0; i < (int)p.size(); i++) {
     cout << p[i] << ' ' << endl;
