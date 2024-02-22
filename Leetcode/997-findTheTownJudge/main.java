a578430520a727151a0a02b8b3cf6b9dba30a7cf
kabbo25 <kabbo25@gmail.com>
2024-02-22 16:34:04 +0600

    using two additinal array , indegree and outDegree

---
+++ Leetcode/997-findTheTownJudge/main.java
@@ -0,0 +1,17 @@
+class Solution {
+  public int findJudge(int n, int[][] trust) {
+    int[] indegree = new int[n + 1];
+    int[] outdegree = new int[n + 1];
+    for (int[] ele : trust) {
+      indegree[ele[1]]++;
+      outdegree[ele[0]]++;
+    }
+    int judge = -1;
+    for (int i = 1; i <= n; ++i) {
+      if (indegree[i] == n - 1 && outdegree[i] == 0) {
+        judge = i;
+      }
+    }
+    return judge;
+  }
+}
