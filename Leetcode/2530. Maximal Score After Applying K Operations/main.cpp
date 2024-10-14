172d3dfc2cdc8fb04adf5e56beb5e168734176cf
kabbo25 <kabbo25@gmail.com>
2024-10-14 13:32:00 +0000

    2530: getting interger overflow

---
+++ Leetcode/2530. Maximal Score After Applying K Operations/main.cpp
@@ -0,0 +1,12 @@
+#include<vector>
+#include <queue>
+using namespace std;
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        int ans=0;
+        priority_queue pq{nums.begin(),nums.end()};
+        return ans;
+
+    }
+};
