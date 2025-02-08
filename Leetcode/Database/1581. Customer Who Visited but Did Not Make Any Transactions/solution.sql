06915dbff0a84e38f0f7d493f9ea9e2779b5597b
kabbo ghosh <kabbo25@gmail.com>
2025-02-09 02:26:21 +0600

    feat(database): Add solution query for LeetCode 1581 - Find customers with no transactions
    Added query using LEFT JOIN and GROUP BY

---
+++ Leetcode/Database/1581. Customer Who Visited but Did Not Make Any Transactions/solution.sql
@@ -0,0 +1,39 @@
+-- Create Visits table
+CREATE TABLE Visits (
+    visit_id INT,
+    customer_id INT
+);
+
+-- Insert data into Visits table
+INSERT INTO Visits (visit_id, customer_id) VALUES
+(1, 23),
+(2, 9),
+(4, 30),
+(5, 54),
+(6, 96),
+(7, 54),
+(8, 54);
+
+-- Create Transactions table
+CREATE TABLE Transactions (
+    transaction_id INT,
+    visit_id INT,
+    amount INT
+);
+
+-- Insert data into Transactions table
+INSERT INTO Transactions (transaction_id, visit_id, amount) VALUES
+(2, 5, 310),
+(3, 5, 300),
+(9, 5, 200),
+(12, 1, 910),
+(13, 2, 970);
+
+SELECT * FROM Visits
+LEFT JOIN leetcode_sql.transactions t on Visits.visit_id = t.visit_id;
+
+SELECT v.customer_id, COUNT(v.customer_id) as count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
